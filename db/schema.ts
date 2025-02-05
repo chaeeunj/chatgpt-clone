96e8c56a9704e7b37b4bb4f2463d389b0a182093
chaeeunj <official2930@naver.com>
2025-02-05 21:29:00 +0900

    user 테이블과 conversation 테이블 사이의 one to many 관계 설정

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,7 @@
 import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 
-export const users = pgTable('users', {
+export const user = pgTable('users', {
   id: uuid('id').defaultRandom().notNull().primaryKey(),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
@@ -10,10 +11,24 @@ export const users = pgTable('users', {
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
 
+export const userRelations = relations(user, ({ many }) => ({
+  conversations: many(conversation),
+}));
+
 export const conversation = pgTable('conversation', {
   id: uuid('id').defaultRandom().notNull().primaryKey(),
   name: text('name'),
+  userId: uuid('userId')
+    .references(() => user.id, { onDelete: 'cascade' })
+    .notNull(),
 
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
+
+export const conversationRelations = relations(conversation, ({ one }) => ({
+  user: one(user, {
+    fields: [conversation.userId],
+    references: [user.id],
+  }),
+}));
